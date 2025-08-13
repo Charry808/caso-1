67029c79d53897aa62650b71a69f7e31407f6b30
Charry808 <davidecharry@javeriana.edu.co>
2025-08-13 12:25:50 -0500

    r3

--- caso-1.r
+++ caso-1.r
@@ -1,1 +1,9 @@
-mean(x, na.rm=FALSE)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(readr)
+library(moments)
+library(lubridate)
+# Cargar los datos
+datos <- read_csv("Web_Anakytics.csv")
+df <- read.csv("Web_Anakytics.csv")
