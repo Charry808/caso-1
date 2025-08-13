cf2949ed650d89fdce56befae2b5763f829242a8
Manuelagranadosh <manuelagranadosh@javeriana.edu.co>
2025-08-13 12:51:59 -0500

    ufff

--- caso-1.r
+++ caso-1.r
@@ -4,6 +4,8 @@ library(tidyr)
 library(readr)
 library(moments)
 library(lubridate)
-# Cargar los datos
-datos <- read_csv("Web_Anakytics.csv")
-df <- read.csv("Web_Anakytics.csv")
+install.packages("moments")
+library(moments)
+setwd("/Users/manuelagranadoshernandez/Documents/GitHub/caso-1/")
+cost_path <- "hoja1caso.csv"
+read.csv(caso_path)
